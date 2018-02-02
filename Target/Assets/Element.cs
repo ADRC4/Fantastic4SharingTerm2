484fd8053e86061b7f7dffb544567a1d09b157dd
Vicente <vicente@visose.com>
2018-02-02 18:56:45 +0000

    Example of drawing spheres on vertices

---
+++ Target/Assets/Element.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Linq;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Element : MonoBehaviour
+{
+    static Mesh _sphere;
+    static Material _material;
+
+    Vector3[] _vertices;
+
+    void Awake()
+    {
+        if (_sphere == null)
+        {
+            var go = GameObject.CreatePrimitive(PrimitiveType.Sphere);
+            _sphere = go.GetComponent<MeshFilter>().mesh;
+            Destroy(go);
+
+            _material = new Material(Shader.Find("Standard"))
+            {
+                color = Color.red,
+                enableInstancing = true
+            };
+        }
+    }
+
+    void Start()
+    {
+        _vertices = GetComponent<MeshFilter>().mesh.vertices;
+    }
+
+    void Update()
+    {
+        if (MainController.DrawVertices)
+            DrawVertices();
+    }
+
+    void DrawVertices()
+    {
+        var matrices = _vertices
+            .Select(v=> Matrix4x4.TRS(transform.TransformPoint(v), Quaternion.identity, Vector3.one * 0.1f))
+            .ToArray();
+
+        Graphics.DrawMeshInstanced(_sphere, 0, _material, matrices);
+    }
+}
---
+++ Target/Assets/MainController.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MainController : MonoBehaviour
+{
+    public static bool DrawVertices = false;
+
+    void OnGUI()
+    {
+        DrawVertices = GUILayout.Toggle(DrawVertices, "Draw vertices");
+    }
+}
